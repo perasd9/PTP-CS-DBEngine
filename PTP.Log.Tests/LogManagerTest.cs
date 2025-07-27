b7ddf9ac6d790107d295b2b58d93c46845c9b0ce
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-07-27 18:25:23 +0200

    start log tests

---
+++ PTP.Log.Tests/LogManagerTest.cs
@@ -0,0 +1,19 @@
+﻿namespace PTP.Log.Tests
+{
+    internal class LogManagerTest : IDisposable
+    {
+        private LogManager _logManager;
+        private string _testFile;
+
+        public LogManagerTest()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            if(Directory.Exists(_testFile))
+                Directory.Delete(_testFile, recursive: true);
+        }
+    }
+}
