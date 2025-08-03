485a9a0e7a6d4e263fd868912c748113349398e9
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-03 10:23:55 +0200

    add checkpoint log record

---
+++ PTP.Transaction/Recovery/CheckpointRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace PTP.Transaction.Recovery
+{
+    internal class CheckpointRecord : ILogRecord
+    {
+        public int Operator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TransactionNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo(int transactionNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
