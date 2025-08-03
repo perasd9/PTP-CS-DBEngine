6c10cad7daaa73fdbb0470124354cc8a10e9f9fd
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-03 10:23:06 +0200

    add log record interface

---
+++ PTP.Transaction/Recovery/ILogRecord.cs
@@ -0,0 +1,38 @@
+﻿using PTP.Disk;
+
+namespace PTP.Transaction.Recovery
+{
+    public interface ILogRecord
+    {
+        public const int CHECKPOINT = 0, START = 1, COMMIT = 2, ROLLBACK = 3, SETINT = 4, SETSTRING = 5;
+
+        public int Operator();
+        public int TransactionNumber();
+        public void Undo(int transactionNumber);
+
+        static ILogRecord CreateLogRecord(byte[] bytes)
+        {
+            Page page = new Page(bytes);
+
+            switch(page.GetInt(0))
+            {
+                case CHECKPOINT:
+                    return new CheckpointRecord();
+                case START:
+                    return new StartRecord(page);
+                case COMMIT:
+                    return new CommitRecord(page);
+                case ROLLBACK:
+                    return new RollbackRecord(page);
+                case SETINT:
+                    return new SetIntRecord(page);
+                case SETSTRING:
+                    return new SetStringRecord(page);
+
+                default:
+                    return null;
+            }
+        }
+
+    }
+}
