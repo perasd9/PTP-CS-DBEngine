676953ec4b92e1b50f323a8e06bffd6b6fc69767
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-03 10:24:04 +0200

    add commit log record

---
+++ PTP.Transaction/Recovery/CommitRecord.cs
@@ -0,0 +1,29 @@
+﻿using PTP.Disk;
+
+namespace PTP.Transaction.Recovery
+{
+    internal class CommitRecord : ILogRecord
+    {
+        private Page page;
+
+        public CommitRecord(Page page)
+        {
+            this.page = page;
+        }
+
+        public int Operator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int TransactionNumber()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo(int transactionNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
