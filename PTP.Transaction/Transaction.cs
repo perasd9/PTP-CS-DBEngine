87b3913bfa92a6598f9b261c076383a31301ffeb
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-04 19:00:46 +0200

    start transaction class

---
+++ PTP.Transaction/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using PTP.Disk;
+
+namespace PTP.Transaction
+{
+    public class Transaction
+    {
+        internal void Pin(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetInt(Block block, int offset, int value, bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetString(Block block, int offset, string value, bool v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Unpin(Block block)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
