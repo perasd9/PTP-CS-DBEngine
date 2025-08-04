4c835abf8b607b1c35c72b0267ba1f48b89e3c23
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-04 18:58:56 +0200

    checkpoint log record update

---
+++ PTP.Transaction/Recovery/Log Records/CheckpointRecord.cs
@@ -0,0 +1,33 @@
+﻿using PTP.Disk;
+using PTP.Log;
+
+namespace PTP.Transaction.Recovery
+{
+    internal class CheckpointRecord : ILogRecord
+    {
+        public int Operator() => ILogRecord.CHECKPOINT;
+
+        public int TransactionNumber()
+        {
+            return -1;
+        }
+
+        public void Undo(Transaction transaction)
+        {
+
+        }
+
+        public static int WriteToLog(LogManager logManager)
+        {
+            byte[] record = new byte[sizeof(int)];
+
+            Page page = new Page(record);
+
+            page.SetInt(0, ILogRecord.CHECKPOINT);
+
+            return logManager.Append(record);
+        }
+
+        public override string ToString() => $"<CHECKPOINT>";
+    }
+}
