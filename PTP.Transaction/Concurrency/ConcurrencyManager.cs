c89b11c80fba034bc08ac62b36a96502c7f1b640
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-17 10:01:14 +0200

    add concurrenctManager for managig global locks

---
+++ PTP.Transaction/Concurrency/ConcurrencyManager.cs
@@ -0,0 +1,44 @@
+﻿using PTP.Disk;
+
+namespace PTP.Transaction.Concurrency
+{
+    public class ConcurrencyManager
+    {
+        private static LockTable _lockTable = new();
+        private Dictionary<Block, string> _locks = new();
+
+        public void SLock(Block block)
+        {
+            if(!_locks.ContainsKey(block))
+            {
+                _lockTable.SLock(block);
+                _locks.Add(block, "S");
+            }
+        }
+
+        public void XLock(Block block)
+        {
+            if(!HasXLock(block))
+            {
+                SLock(block);
+                _lockTable.XLock(block);
+                _locks.Add(block, "X");
+            }
+        }
+
+        public void Release()
+        {
+            foreach(var block in _locks.Keys)
+                _lockTable.Unlock(block);
+
+            _locks.Clear();
+        }
+        private bool HasXLock(Block block)
+        {
+            _locks.TryGetValue(block, out string lockType);
+
+            return lockType != null && lockType == "X";
+        }
+
+    }
+}
