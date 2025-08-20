6470d54f132d2cf5742dc6cec22f051c3ae91bbe
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-20 18:02:18 +0200

    update description for methods in LogEnumerator class

--- PTP.Log/LogEnumerator.cs
+++ PTP.Log/LogEnumerator.cs
@@ -13,6 +13,11 @@ namespace PTP.Log
         private int _boundary;
         private byte[] _current;
 
+        /*
+         * Block in memory used by the LogManager, represents file which working with this block.
+         * Initialize {_page} as block in memory and important one is that {_page} is EMPTY(just byte array with defaul value).
+         * MoveToBlock will not literally move anything to block but actually read block from file and write into page.
+         */
         public LogEnumerator(FileManager fileManager, Block block)
         {
             _fileManager = fileManager;
@@ -22,6 +27,9 @@ namespace PTP.Log
             MoveToBlock(_block);
         }
 
+        /*
+         * Active item in collection.
+         */
         object IEnumerator.Current => Current;
         public byte[] Current => _current;
 
@@ -31,6 +39,13 @@ namespace PTP.Log
             //nothing to clean up
         }
 
+        /*
+         * If {_currentPosition}(which is 0 by deault at the first calling constructor) is equals with the block size(which means that current is at the end of block)
+         * and if block number is 0(this is important because 0 is actually last block, see log manager where we described that log enumerator starts from the end)
+         * that means that there are no more blocks and return false, if block number is not 0 it can be continued with new block(one after currently which is currently - 1)
+         * and MoveToBlock to actually load that block from disk into memory(Page), in both case if {_currentPosition} was equals to {_fileManager.BlockSize} or not
+         * it's going on read next data with GetBytes method, that's {_current}, {_currentPosition} is also updated with new offset and it returns true(go to next item)
+         */
         public bool MoveNext()
         {
             if(_currentPosition == _fileManager.BlockSize)
@@ -54,6 +69,11 @@ namespace PTP.Log
             //we go in reverse order, so we have nothing to reset
             throw new NotSupportedException();
         }
+
+        /*
+         * This method will read block from disk and load that into {_page} variable.
+         * {_boundary} will just be read as first 4B(int), that's boundary of 
+         */
         private void MoveToBlock(Block block)
         {
             _fileManager.Read(block, _page);
