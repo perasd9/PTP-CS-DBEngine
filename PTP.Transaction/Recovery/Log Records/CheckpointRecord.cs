2ba684c17c8f5adc5339d32862a57e065e9985f3
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-23 15:10:55 +0200

    update description on method in CheckpointRecord

--- PTP.Transaction/Recovery/Log Records/CheckpointRecord.cs
+++ PTP.Transaction/Recovery/Log Records/CheckpointRecord.cs
@@ -7,16 +7,23 @@ namespace PTP.Transaction.Recovery
     {
         public int Operator() => ILogRecord.CHECKPOINT;
 
+        //There is no transaction for checkpoint log record, only purpose of this record is for recovery, to mark til this point is everything right.
         public int TransactionNumber()
         {
             return -1;
         }
 
+        /*
+         * There is no undo action for checkpoint log record.
+         */
         public void Undo(Transaction transaction)
         {
 
         }
 
+        /*
+         * Checkpoint record is made only from the 4B for operator so page with sizeof(int) is made, return value is returned latest LSN by append method.
+         */
         public static int WriteToLog(LogManager logManager)
         {
             byte[] record = new byte[sizeof(int)];
