c29cb9485d3c85806c6da9f278b9d4c3c5f837b9
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-23 15:11:27 +0200

    update description on methods in ILogRecordInterface

--- PTP.Transaction/Recovery/Log Records/ILogRecord.cs
+++ PTP.Transaction/Recovery/Log Records/ILogRecord.cs
@@ -4,12 +4,19 @@ namespace PTP.Transaction.Recovery
 {
     public interface ILogRecord
     {
+        //Message types in log file.
         public const int CHECKPOINT = 0, START = 1, COMMIT = 2, ROLLBACK = 3, SETINT = 4, SETSTRING = 5;
 
+        //Every log record(implementation of interface) will return one of message type.
         public int Operator();
+        //From start transaction til the end for every message it has to be known to which transaction belongs.
         public int TransactionNumber();
+        //In this db system decision for recovery is UNDO only approach, so this is compesantion like operation, back action on old value.
         public void Undo(Transaction transaction);
 
+        /*
+         * Based on first 4B(int) create conrete implementation instance of some Log Record.
+         */
         static ILogRecord CreateLogRecord(byte[] bytes)
         {
             Page page = new Page(bytes);
