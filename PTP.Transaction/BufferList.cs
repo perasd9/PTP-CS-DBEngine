eaf8b9ee6a5b5da0d8242ad4982236150daa3166
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-26 22:53:22 +0200

    add BufferList as buffers refered only by transaction

---
+++ PTP.Transaction/BufferList.cs
@@ -0,0 +1,35 @@
+﻿using PTP.Buffer;
+using PTP.Disk;
+
+namespace PTP.Transaction
+{
+    internal class BufferList
+    {
+        private BufferManager _bufferManager;
+
+        public BufferList(BufferManager bufferManager)
+        {
+            this._bufferManager = bufferManager;
+        }
+
+        internal Buffer.Buffer GetBuffer(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Pin(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Unpin(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UnpinAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
