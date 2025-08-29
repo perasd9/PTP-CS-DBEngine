4005353543d56a468f7a3594181d1d38cb777583
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-29 19:35:44 +0200

    update recovery manager

--- PTP.Transaction/Recovery/RecoveryManager.cs
+++ PTP.Transaction/Recovery/RecoveryManager.cs
@@ -6,6 +6,9 @@ namespace PTP.Transaction.Recovery
 {
     public class RecoveryManager
     {
+        /*
+         * Recovery manager deals only with logs in log file and based on that logs execute actions, 
+         */
         private readonly LogManager _logManager;
         private readonly BufferManager _bufferManager;
         private readonly Transaction _transaction;
@@ -18,9 +21,13 @@ namespace PTP.Transaction.Recovery
             _transaction = transaction;
             _transactionNumber = transactionNumber;
 
-            StartRecord.WriteToLog(_logManager, transactionNumber);
+            StartRecord.WriteToLog(_logManager, transactionNumber); //on constructor it's logical that first log is START
         }
 
+        /*
+         * This method will flush transaction's buffer, it seems like it crash rule of WAL but in FlushAll method firsly logs are written and then content.
+         * After that Commit record is written to log and LSN which is last will be flushed.
+         */
         public void Commit()
         {
             _bufferManager.FlushAll(this._transactionNumber);
@@ -28,6 +35,10 @@ namespace PTP.Transaction.Recovery
             _logManager.Flush(LSN);
         }
 
+        /*
+         * This method will firstly does undo action for all records(see DoRollBack), after that is same as above, flush everything of transaction,
+         * write Rollback recrod in log and flush that also.
+         */
         public void Rollback()
         {
             DoRollback();
@@ -36,6 +47,9 @@ namespace PTP.Transaction.Recovery
             _logManager.Flush(LSN);
         }
 
+        /*
+         * This method will as in rollback do the recover with DoRecover method and flush all, write Dheckpoint record and flush that also.
+         */
         public void Recover()
         {
             DoRecover();
@@ -44,6 +58,9 @@ namespace PTP.Transaction.Recovery
             _logManager.Flush(LSN);
         }
 
+        /*
+         * This method is used from transaction, just delegating obligations, it's writing record of SetInt record 
+         */
         public int SetInt(Buffer.Buffer buffer, int offset, int newValue)
         {
             int oldValue = buffer.Contents.GetInt(offset);
@@ -52,6 +69,9 @@ namespace PTP.Transaction.Recovery
             return SetIntRecord.WriteToLog(_logManager, _transactionNumber, block, offset, oldValue); 
         }
 
+        /*
+         * This method is used from transaction, just delegating obligations, it's writing record of SetString record 
+         */
         public int SetString(Buffer.Buffer buffer, int offset, string newValue)
         {
             string oldValue = buffer.Contents.GetString(offset);
@@ -60,6 +80,11 @@ namespace PTP.Transaction.Recovery
             return SetStringRecord.WriteToLog(_logManager, _transactionNumber, block, offset, oldValue);
         }
 
+        /*
+         * With enumerator(which starts from backward this is really important) iterating through logs based on that logs 
+         * ILogRecord(of course implementation class of interface) creates concrete record, and really important part is that if 
+         * that record belongs to this transaction number and if operator is not START it should undo action of record.
+         */
         private void DoRollback()
         {
             IEnumerator<byte[]> enumerator = _logManager.Enumerator();
@@ -70,12 +95,21 @@ namespace PTP.Transaction.Recovery
 
                 ILogRecord logRecord = ILogRecord.CreateLogRecord(bytes);
 
-                if(logRecord.TransactionNumber() == this._transactionNumber)
-                    if(logRecord.Operator() == ILogRecord.START)
-                        logRecord.Undo(_transaction);
+                if (logRecord.TransactionNumber() == this._transactionNumber)
+                {
+                    if (logRecord.Operator() == ILogRecord.START)
+                        return;
+
+                    logRecord.Undo(_transaction);
+                }
             }
         }
 
+        /*
+         * This method will do concrete recover actions, firslt is made empty list of finished transactions(that is important because finished transactions
+         * don't need to be recovered or whatever else). As in DoRollback enumerator will iterate through logs from backward, when hit Commit or Rollback record
+         * it adds transaction into finished and goes to another records whose transactions are not finished and undo them until next transaction.
+         */
         private void DoRecover()
         {
             IList<int> finishedTransactions = new List<int>();
