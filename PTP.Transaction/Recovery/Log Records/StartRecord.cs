9610876e64d4e14cf1381c9f13da3db61f3adfa1
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-29 19:35:57 +0200

    update description of methods in StartRecord log record

--- PTP.Transaction/Recovery/Log Records/StartRecord.cs
+++ PTP.Transaction/Recovery/Log Records/StartRecord.cs
@@ -5,8 +5,12 @@ namespace PTP.Transaction.Recovery
 {
     internal class StartRecord : ILogRecord
     {
+        //Describe which transaction this record belongs.
         private int _transactionNumber;
 
+        /*
+         * After first 4B(int) for operator, second 4B(int) is transaction number so it's read like this.
+         */
         public StartRecord(Page page)
         {
             int transactionPosition = sizeof(int);
@@ -17,11 +21,18 @@ namespace PTP.Transaction.Recovery
 
         public int TransactionNumber() => _transactionNumber;
 
+        /*
+         * There is no undo action for commit log record.
+         */
         public void Undo(Transaction transaction)
         {
 
         }
 
+        /*
+         * Start record is made by operator on first 4B and transaction number on second 4B so page of 2 * sizeof(int) is created by that idea.
+         * At the end this method returns returned value from logManager.Append method and this is important because returend value is latest LSN.
+         */
         public static int WriteToLog(LogManager logManager, int transactionNumber)
         {
             byte[] record = new byte[2 * sizeof(int)];
