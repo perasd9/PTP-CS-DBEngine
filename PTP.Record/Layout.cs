7d685c258588aa5fb874fe26e6c3f0268b39f5f6
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-30 11:55:27 +0200

    add Layout class for metadata

---
+++ PTP.Record/Layout.cs
@@ -0,0 +1,49 @@
+﻿using PTP.Disk;
+
+namespace PTP.Shared
+{
+    public class Layout
+    {
+        private readonly Schema _schema;
+        private IDictionary<string, int> _offsets;
+        private int _slotSize;
+
+        public Layout(Schema schema)
+        {
+            _schema = schema;
+            _offsets = new Dictionary<string, int>();
+            int pos = sizeof(int); // First 4B reserved for flag which represents if record is deleted or not
+
+            foreach (var field in schema.Fields())
+            {
+                _offsets[field] = pos;
+                pos += this.LengthInBytes(field);
+            }
+
+            _slotSize = pos;
+        }
+
+        public Layout(Schema schema, IDictionary<string, int> offsets, int slotSize)
+        {
+            _schema = schema;
+            _offsets = offsets;
+            _slotSize = slotSize;
+        }
+
+        public Schema Schema => _schema;
+
+        public int Offset(string fieldName)
+        {
+            return _offsets[fieldName];
+        }
+
+        public int SlotSize => _slotSize;
+        private int LengthInBytes(string field)
+        {
+            FieldType type = (FieldType)_schema.Type(field);
+
+            if (type == FieldType.INT) return sizeof(int);
+            else return Page.MaxLength(_schema.Length(field));
+        }
+    }
+}
