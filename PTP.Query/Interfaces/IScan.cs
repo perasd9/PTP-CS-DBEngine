122293e6d3720346594af6d3f7b612b5be9973c4
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-31 08:50:00 +0200

    add IScan interface for only selectable items

---
+++ PTP.Query/Interfaces/IScan.cs
@@ -0,0 +1,13 @@
+﻿namespace PTP.Query.Interfaces
+{
+    public interface IScan
+    {
+        public void BeforeFirst();
+        public bool Next();
+        public int GetInt(string fieldName);
+        public string GetString(string fieldName);
+        public Constant GetValue(string fieldName);
+        public bool HasField(string fieldName);
+        public void Close();
+    }
+}
