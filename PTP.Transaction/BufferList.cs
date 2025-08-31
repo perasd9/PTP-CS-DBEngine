2771a4012e8582bd10058417973f2c0107544c1c
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-31 13:44:50 +0200

    update BufferList in transaction

--- PTP.Transaction/BufferList.cs
+++ PTP.Transaction/BufferList.cs
@@ -3,8 +3,10 @@ using PTP.Disk;
 
 namespace PTP.Transaction
 {
-    internal class BufferList
+    public class BufferList
     {
+        private Dictionary<Block, Buffer.Buffer> _bufferPool = new Dictionary<Block, Buffer.Buffer>();
+        private ICollection<Block> _pinnedBlocks = new List<Block>();
         private BufferManager _bufferManager;
 
         public BufferList(BufferManager bufferManager)
@@ -12,24 +14,41 @@ namespace PTP.Transaction
             this._bufferManager = bufferManager;
         }
 
-        internal Buffer.Buffer GetBuffer(Block block)
+        public Buffer.Buffer GetBuffer(Block block)
         {
-            throw new NotImplementedException();
+            return _bufferPool.GetValueOrDefault(block);
         }
 
-        internal void Pin(Block block)
+        public void Pin(Block block)
         {
-            throw new NotImplementedException();
+            Buffer.Buffer buffer = _bufferManager.Pin(block);
+
+            _bufferPool[block] = buffer;
+
+            _pinnedBlocks.Add(block);
         }
 
-        internal void Unpin(Block block)
+        public void Unpin(Block block)
         {
-            throw new NotImplementedException();
+            var doesExist = _bufferPool.TryGetValue(block, out var buffer);
+
+            _bufferManager.Unpin(buffer);
+            _pinnedBlocks.Remove(block);
+
+            if(!_pinnedBlocks.Contains(block))
+                _bufferPool.Remove(block);
         }
 
-        internal void UnpinAll()
+        public void UnpinAll()
         {
-            throw new NotImplementedException();
+            foreach(var block in _pinnedBlocks.ToList())
+            {
+                Buffer.Buffer buffer = _bufferPool[block];
+                _bufferManager.Unpin(buffer);
+            }
+
+            _bufferPool.Clear();
+            _pinnedBlocks.Clear();
         }
     }
 }
