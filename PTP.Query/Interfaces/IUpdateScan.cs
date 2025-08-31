53f3c1561f29dd305fec54b432329112b8e87bfd
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-08-31 08:50:20 +0200

    add IUpdateScan interface for updatable items

---
+++ PTP.Query/Interfaces/IUpdateScan.cs
@@ -0,0 +1,15 @@
+﻿using PTP.Shared;
+
+namespace PTP.Query.Interfaces
+{
+    public interface IUpdateScan : IScan
+    {
+        public void SetInt(string fieldName, int value);
+        public void SetString(string fieldName, string value);
+        public void SetValue(string fieldName, Constant value);
+        public void Insert();
+        public void Delete();
+        public RID GetRID();
+        public void MoveToRID(RID rid);
+    }
+}
