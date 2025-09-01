d1957599fc8c83863db68c2c87454d1fa08361e9
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-01 21:25:14 +0200

    add ProjectScan as projection on table

---
+++ PTP.Query/ProjectScan.cs
@@ -0,0 +1,60 @@
+﻿using PTP.Query.Interfaces;
+
+namespace PTP.Query
+{
+    public class ProjectScan : IScan
+    {
+        private IScan _scan;
+        private ICollection<string> _fields;
+
+        public ProjectScan(IScan scan, List<string> fields)
+        {
+            _scan = scan;
+            _fields = fields;
+        }
+
+        public void BeforeFirst()
+        {
+            _scan.BeforeFirst();
+        }
+
+        public void Close()
+        {
+            _scan.Close();
+        }
+
+        public int GetInt(string fieldName)
+        {
+            if(this.HasField(fieldName))
+                return _scan.GetInt(fieldName);
+            else
+                throw new ArgumentException($"Field {fieldName} not found.");
+        }
+
+        public string GetString(string fieldName)
+        {
+            if (this.HasField(fieldName))
+                return _scan.GetString(fieldName);
+            else
+                throw new ArgumentException($"Field {fieldName} not found.");
+        }
+
+        public Constant GetValue(string fieldName)
+        {
+            if (this.HasField(fieldName))
+                return _scan.GetValue(fieldName);
+            else
+                throw new ArgumentException($"Field {fieldName} not found.");
+        }
+
+        public bool HasField(string fieldName)
+        {
+            return _fields.Contains(fieldName);
+        }
+
+        public bool Next()
+        {
+            return _scan.Next();
+        }
+    }
+}
