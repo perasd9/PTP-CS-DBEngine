93a66c9e4f0e42e4371a853a5f30442b06f578a3
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-10 20:27:46 +0200

    add metadata manager

---
+++ PTP.Metadata/MetadataManager.cs
@@ -0,0 +1,19 @@
+﻿namespace PTP.Metadata
+{
+    public class MetadataManager
+    {
+        private static TableManager _tableManager;
+        private static StatisticManager _statisticManager;
+        private static ViewManager _viewManager;
+        private static IndexManager _indexManager;
+
+        public MetadataManager(bool isNew, Transaction.Transaction transaction)
+        {
+            _tableManager = new TableManager(isNew, transaction);
+            _statisticManager = new StatisticManager(_tableManager, transaction);
+            _viewManager = new ViewManager(isNew, _tableManager, transaction);
+            _indexManager = new IndexManager(isNew, _tableManager, transaction);
+
+        }
+    }
+}
