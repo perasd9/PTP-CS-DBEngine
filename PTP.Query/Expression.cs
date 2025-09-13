7e862aff793d7172135fc0805ed6e4fa1348ee29
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-13 20:52:54 +0200

    add Expression class for queries

---
+++ PTP.Query/Expression.cs
@@ -0,0 +1,44 @@
+﻿using PTP.Query.Interfaces;
+using PTP.Shared;
+
+namespace PTP.Query
+{
+    public class Expression
+    {
+        private Constant _value = null;
+        private string _fieldName = null;
+
+        public Expression(Constant value)
+        {
+            _value = value;
+        }
+        public Expression(string fieldName)
+        {
+            _fieldName = fieldName;
+        }
+
+        public bool IsFieldName()
+        {
+            return _fieldName != null;
+        }
+
+        public Constant AsConstant => _value;
+
+        public string AsFieldName => _fieldName;
+
+        public Constant Evaluate(IScan scan)
+        {
+            return _value != null ? _value : scan.GetValue(_fieldName);
+        }
+
+        public bool AppliesTo(Schema schema)
+        {
+            return _value != null ? true : schema.HasField(_fieldName);
+        }
+
+        public override string ToString()
+        {
+            return _value != null ? _value.ToString() : _fieldName;
+        }
+    }
+}
