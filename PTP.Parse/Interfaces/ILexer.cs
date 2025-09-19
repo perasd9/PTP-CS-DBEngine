14550674a205b678a8e6cba04a76c6054cb2b57f
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-19 19:05:25 +0200

    add lexer interface

---
+++ PTP.Parse/Interfaces/ILexer.cs
@@ -0,0 +1,20 @@
+﻿using PTP.Parse.Custom_Tokenizer;
+
+namespace PTP.Parse.Interfaces
+{
+    public interface ILexer
+    {
+        public ICollection<string> Keywords { get; }
+        public SimpleTokenizer Tokenizer { get; }
+        public bool MatchDelim(char delimiter);
+        public bool MatchIntConstant();
+        public bool MatchStringConstant();
+        public bool MatchKeyword(string word);
+        public bool MatchId();
+        public void EatDelim(char delimiter);
+        public int EatIntConstant();
+        public string EatStringConstant();
+        public void EatKeyword(string word);
+        public string EatId();
+    }
+}
