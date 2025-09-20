f41e9bf3a55a3f9d42a50374e03ff663496beb31
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-20 17:51:24 +0200

    add data for create view

---
+++ PTP.Parse/Data/CreateViewData.cs
@@ -0,0 +1,19 @@
+﻿namespace PTP.Parse.Data
+{
+    public class CreateViewData
+    {
+        public CreateViewData(string viewName, QueryData queryData)
+        {
+            ViewName = viewName;
+            QueryData = queryData;
+        }
+
+        public string ViewName { get; }
+        public QueryData QueryData { get; }
+
+        public string ViewDefinition()
+        {
+            return QueryData.ToString();
+        }
+    }
+}
