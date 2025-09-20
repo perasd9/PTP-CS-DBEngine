9ee87707fee79e17b5b53303e838f856b0d17bdb
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-20 17:51:03 +0200

    add data for create index

---
+++ PTP.Parse/Data/CreateIndexData.cs
@@ -0,0 +1,16 @@
+﻿namespace PTP.Parse.Data
+{
+    public class CreateIndexData
+    {
+        public CreateIndexData(string indexName, string tableName, string fieldName)
+        {
+            IndexName = indexName;
+            TableName = tableName;
+            FieldName = fieldName;
+        }
+
+        public string IndexName { get; }
+        public string TableName { get; }
+        public string FieldName { get; }
+    }
+}
