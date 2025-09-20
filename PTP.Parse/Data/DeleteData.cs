e7157b9126b31d5570bc6a202afcee46a854e0b1
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-20 17:51:32 +0200

    add data for delete data

---
+++ PTP.Parse/Data/DeleteData.cs
@@ -0,0 +1,16 @@
+﻿using PTP.Query;
+
+namespace PTP.Parse.Data
+{
+    public class DeleteData
+    {
+        public DeleteData(string tableName, Predicate pred)
+        {
+            TableName = tableName;
+            Pred = pred;
+        }
+
+        public string TableName { get; }
+        public Predicate Pred { get; }
+    }
+}
