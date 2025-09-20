5a526bf56bb5c67bb6654bbba94dd6a4f58653f3
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-20 17:51:17 +0200

    add data for create table

---
+++ PTP.Parse/Data/CreateTableData.cs
@@ -0,0 +1,16 @@
+﻿using PTP.Shared;
+
+namespace PTP.Parse.Data
+{
+    public class CreateTableData
+    {
+        public CreateTableData(string tableName, Schema schema)
+        {
+            TableName = tableName;
+            Schema = schema;
+        }
+
+        public string TableName { get; }
+        public Schema Schema { get; }
+    }
+}
