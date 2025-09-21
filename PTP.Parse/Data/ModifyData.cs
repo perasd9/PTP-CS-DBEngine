5df41b6656d7cf98181baebdc62d4755590ae08e
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-21 22:27:24 +0200

    add modify data for query

---
+++ PTP.Parse/Data/ModifyData.cs
@@ -0,0 +1,20 @@
+﻿using PTP.Query;
+
+namespace PTP.Parse.Data
+{
+    public class ModifyData
+    {
+        public ModifyData(string tableName, string fieldName, Expression newValue, Predicate pred)
+        {
+            TableName = tableName;
+            FieldName = fieldName;
+            NewValue = newValue;
+            Pred = pred;
+        }
+
+        public string TableName { get; }
+        public string FieldName { get; }
+        public Expression NewValue { get; }
+        public Predicate Pred { get; }
+    }
+}
