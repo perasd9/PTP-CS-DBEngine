dbbd1837b9b1899faad27e3f19e25f1e412af3c5
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-21 22:27:12 +0200

    add insert data for query

---
+++ PTP.Parse/Data/InsertData.cs
@@ -0,0 +1,18 @@
+﻿using PTP.Query;
+
+namespace PTP.Parse.Data
+{
+    public class InsertData
+    {
+        public InsertData(string tableName, List<string> fields, List<Constant> values)
+        {
+            TableName = tableName;
+            Fields = fields;
+            Values = values;
+        }
+
+        public string TableName { get; }
+        public List<string> Fields { get; }
+        public List<Constant> Values { get; }
+    }
+}
