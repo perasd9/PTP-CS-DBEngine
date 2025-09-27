1310abf44ebc53d8d75faf83cae81791a3f5206c
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-09-27 15:59:44 +0200

    add plan interface

---
+++ PTP.Query/Interfaces/IPlan.cs
@@ -0,0 +1,14 @@
+﻿using PTP.Query.Interfaces;
+using PTP.Shared;
+
+namespace PTP.Query
+{
+    public interface IPlan
+    {
+        public IScan Open();
+        public int BlocksAccessed();
+        public int RecordsOutput();
+        public int DistinctValues(string fieldName);
+        public Schema Schema();
+    }
+}
