c2003432fb78427a8812171de836acd164769b97
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-10-01 21:54:26 +0200

    add query planner interface

---
+++ PTP.QueryPlan/Planners/Interfaces/IQueryPlanner.cs
@@ -0,0 +1,10 @@
+﻿using PTP.Parse.Data;
+using PTP.Query;
+
+namespace PTP.QueryPlan.Planners.Interfaces
+{
+    public interface IQueryPlanner
+    {
+        public IPlan CreatePlan(QueryData queryData, Transaction.Transaction transaction);
+    }
+}
