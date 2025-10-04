76529c46c99f8c75e500adb8400c2805bb959489
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-10-04 09:14:50 +0200

    add update planner interface

---
+++ PTP.QueryPlan/Planners/Interfaces/IUpdatePlanner.cs
@@ -0,0 +1,15 @@
+﻿using PTP.Parse.Data;
+
+namespace PTP.QueryPlan.Planners.Interfaces
+{
+    public interface IUpdatePlanner
+    {
+        public int ExecuteInsert(InsertData data, Transaction.Transaction tx);
+        public int ExecuteDelete(DeleteData data, Transaction.Transaction tx);
+        public int ExecuteModify(ModifyData data, Transaction.Transaction tx);
+        public int ExecuteCreateTable(CreateTableData data, Transaction.Transaction tx);
+        public int ExecuteCreateView(CreateViewData data, Transaction.Transaction tx);
+        public int ExecuteCreateIndex(CreateIndexData data, Transaction.Transaction tx);
+
+    }
+}
