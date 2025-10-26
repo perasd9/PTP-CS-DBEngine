975f72ef3ab6acf5964e9057fe8a7f764f8119ae
Petar Nikodijević <petar.nikodijevic@asseco-see.rs>
2025-10-26 20:21:30 +0100

    add main planner

---
+++ PTP.QueryPlan/MainPlanner.cs
@@ -0,0 +1,52 @@
+﻿using PTP.Parse;
+using PTP.Parse.Data;
+using PTP.Query;
+using PTP.QueryPlan.Planners.Interfaces;
+
+namespace PTP.QueryPlan
+{
+    public class MainPlanner
+    {
+        private IQueryPlanner _queryPlanner;
+        private IUpdatePlanner _updatePlanner;
+
+        public MainPlanner(IQueryPlanner queryPlanner, IUpdatePlanner updatePlanner)
+        {
+            _queryPlanner = queryPlanner;
+            _updatePlanner = updatePlanner;
+        }
+
+
+        public IPlan CreatePlan(string qry, Transaction.Transaction transaction)
+        {
+            Parser parser = new Parser(qry);
+            QueryData data = parser.Query();
+
+            //this.VerifyQuery(data);
+
+            return _queryPlanner.CreatePlan(data, transaction);
+        }
+
+        public int ExecuteUpdate(string cmd, Transaction.Transaction transaction)
+        {
+            Parser parser = new Parser(cmd);
+            object data = parser.UpdateCommand();
+            //this.VerifyUpdate(data);
+
+            if (data is InsertData insertData)
+                return _updatePlanner.ExecuteInsert(insertData, transaction);
+            else if (data is DeleteData deleteData)
+                return _updatePlanner.ExecuteDelete(deleteData, transaction);
+            else if (data is ModifyData modifyData)
+                return _updatePlanner.ExecuteModify(modifyData, transaction);
+            else if (data is CreateTableData createTableData)
+                return _updatePlanner.ExecuteCreateTable(createTableData, transaction);
+            else if (data is CreateViewData createViewData)
+                return _updatePlanner.ExecuteCreateView(createViewData, transaction);
+            else if (data is CreateIndexData createIndexData)
+                return _updatePlanner.ExecuteCreateIndex(createIndexData, transaction);
+            else
+                return 0;
+        }
+    }
+}
